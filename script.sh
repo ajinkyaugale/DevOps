23cf5ff3f8874eb915c908cc375f5dca1a5cbecf
root <root@jenkinslab-kvtd.us-central1-a.c.thinknyxproject.internal>
2019-07-17 08:52:00 +0000

    Added script

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "This is my script"
+mkdir /yogeshdir
+echo "Directory Created"
+touch /yogeshdir/yogeshfile
+echo "File Created"
+yum -y install telnet
+echo "##################"
+echo "####SUCESSSSSSS###"
+echo "##################"
